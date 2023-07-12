37b1d9e8c542b2092e6d5b25203dcedbc937a5f6
Nowa <135564143+Nowa771@users.noreply.github.com>
2023-07-12 14:43:52 +1000

    Updated the randomizer script

--- Assets/Script/RandomizerScript.cs
+++ Assets/Script/RandomizerScript.cs
@@ -4,18 +4,24 @@ using UnityEngine;
 
 public class RandomizerScript : MonoBehaviour
 {
+    public string[] stockSymbols; // Add the Stock symbls
+    public float minStockPrice; // Add the minimum stock price
+    public float maxStockPrice; // Add the maximum Stock price
 
-    public float Randum = 0f;
+    
+    
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        foreach (string symbol in stockSymbols)
+        {
+            // Generate a random stoick price between minStockPrice and maxStock price
+            float stockPrice = Random.Range(minStockPrice, maxStockPrice);
+
+            Debug.Log(symbol + " " + stockPrice.ToString("C"));
+        }
+
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        Randum = Random.Range(-20, 20);
-        Debug.Log(Randum);
-    }
 }
