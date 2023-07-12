43f6694a587cc886799eeb46c2d62b38c6f74e49
BunBaoi <135566105+BunBaoi@users.noreply.github.com>
2023-07-12 13:47:15 +1000

    Day Timer
    Reason
    - To make randomizer work needs day timer
    - Everytime day timer updates it would update the randomizer chnaging the stock price

---
+++ Assets/Script/TimerScript.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimerScript : MonoBehaviour
+{
+    [Header("Day Variables")]
+    public float dayLength; // Day: True/False
+    public Text DayText; // Day Counter
+
+
+
+
+    // Hidden Variables
+    [HideInInspector]
+    public bool gameStarted, dayOver;
+    [HideInInspector]
+    public float currentTime, currentMoney;
+    [HideInInspector]
+    public int currentDay;
+
+
+
+    void Start()
+    {
+        gameStarted = true;
+        dayOver = false;
+    }
+
+    
+    void Update()
+    {
+        NewDate(); // Day Counter
+    }
+
+    private void NewDate()
+    {
+        if (dayOver == true) // When dayOver is true it adds another day
+        {
+            currentTime = dayLength;
+            currentDay++;
+            DayText.text = "Day: " + currentDay;
+            NextDay();
+        }
+        Timer();
+    }
+
+    private void Timer() // If timer is 0 -> set dayOver = true
+    {
+        if (dayOver == false && gameStarted == true)
+        {
+            currentTime -= Time.deltaTime;
+            if (currentTime <= 0)
+                dayOver = true;
+        }
+    }
+
+    private void NextDay() // Resets dayOver back to false
+    {
+        Debug.Log("Day");
+        dayOver = false;
+    }
+
+}
