ebaf96ccc8f8bded512d43ad22b2dcc14153a227
BunBaoi <135566105+BunBaoi@users.noreply.github.com>
2023-07-14 13:22:03 +1000

    UI Update ver 1

--- Assets/Script/RandomizerScript.cs
+++ Assets/Script/RandomizerScript.cs
@@ -11,7 +11,6 @@ public class RandomizerScript : MonoBehaviour
     
     
     
-    // Start is called before the first frame update
     void Start()
     {
         foreach (string symbol in stockSymbols)
